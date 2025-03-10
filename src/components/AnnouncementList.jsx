e160be8babd24d027d26ee94d609eff90bc3bbd7
arif.ashraf <arif.ashraf@helpshift.com>
2025-03-10 16:22:58 +0530

    Add anouncement changes

--- src/components/AnnouncementList.jsx
+++ src/components/AnnouncementList.jsx
@@ -7,6 +7,13 @@ import Announcement from "./Announcement";
 
 const announcements = [
   {
+    date: "10 Mar 2025",
+    title: "SDK X Unreal 10.4.0 released",
+    info: `SDK X Unreal 10.4.0 is now released for iOS and Android! Upgrade to get the latest features`,
+    link: "/sdkx-unreal",
+    linkTitle: "SDK X Unreal 10.4.0",
+  },
+  {
     date: "27 Nov 2024",
     title: "SDK X Android 10.4.0 released",
     info: `SDK X Android 10.4.0 is now released! Upgrade to get the latest features`,
@@ -57,7 +64,7 @@ const announcements = [
     date: "10 Oct 2022",
     title: "SDK X 10.2.0 released",
     info: `SDK X 10.2.0 is now released for iOS and Android! Upgrade to get the latest features`,
-  }
+  },
 ];
 
 export default function AnnouncementList() {
